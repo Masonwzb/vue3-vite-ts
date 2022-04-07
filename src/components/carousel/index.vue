675242c7579f04d5239b390159852e379515e114
Administrator <bineawu@163.com>
2022-04-07 15:37:58 +0800

    feat: carousel test completed

--- src/components/carousel/index.vue
+++ src/components/carousel/index.vue
@@ -1,114 +1,130 @@
 <template>
-  <div class="carousel-box">
-    <div class="carousel-content">
-      <div class="data slick-initialized slick-slider"
-           data-slick="{&quot;slidesToShow&quot;: 4, &quot;slidesToScroll&quot;: 4}">
-        <button type="button" data-role="none" class="slick-prev slick-arrow" aria-label="Previous" role="button"
-                style="display: block;" @click="previousClick">Previous
-        </button>
-        <div aria-live="polite" class="slick-list draggable">
-          <div class="slick-track" :style="[baseStyle, otherStyle]" role="listbox">
-            <div class="slick-slide slick-cloned" data-slick-index="-4" aria-hidden="true" style="width: 140px;"
-                 tabindex="-1">
-              <h3>3</h3>
-            </div>
-            <div class="slick-slide slick-cloned" data-slick-index="-3" aria-hidden="true" style="width: 140px;"
-                 tabindex="-1">
-              <h3>4</h3>
-            </div>
-            <div class="slick-slide slick-cloned" data-slick-index="-2" aria-hidden="true" style="width: 140px;"
-                 tabindex="-1">
-              <h3>5</h3>
-            </div>
-            <div class="slick-slide slick-cloned" data-slick-index="-1" aria-hidden="true" style="width: 140px;"
-                 tabindex="-1">
-              <h3>6</h3>
-            </div>
-            <div class="slick-slide slick-current slick-active" data-slick-index="0" aria-hidden="false"
-                 style="width: 140px;" tabindex="-1" role="option" aria-describedby="slick-slide30">
-              <h3>1</h3>
-            </div>
-            <div class="slick-slide slick-active" data-slick-index="1" aria-hidden="false" style="width: 140px;"
-                 tabindex="-1" role="option" aria-describedby="slick-slide31">
-              <h3>2</h3>
-            </div>
-            <div class="slick-slide slick-active" data-slick-index="2" aria-hidden="false" style="width: 140px;"
-                 tabindex="-1" role="option" aria-describedby="slick-slide32">
-              <h3>3</h3>
-            </div>
-            <div class="slick-slide slick-active" data-slick-index="3" aria-hidden="false" style="width: 140px;"
-                 tabindex="-1" role="option" aria-describedby="slick-slide33">
-              <h3>4</h3>
-            </div>
-            <div class="slick-slide" data-slick-index="4" aria-hidden="true" style="width: 140px;" tabindex="-1"
-                 role="option" aria-describedby="slick-slide34">
-              <h3>5</h3>
-            </div>
-            <div class="slick-slide" data-slick-index="5" aria-hidden="true" style="width: 140px;" tabindex="-1"
-                 role="option" aria-describedby="slick-slide35">
-              <h3>6</h3>
-            </div>
-            <div class="slick-slide slick-cloned" data-slick-index="6" aria-hidden="true" style="width: 140px;"
-                 tabindex="-1">
-              <h3>1</h3>
-            </div>
-            <div class="slick-slide slick-cloned" data-slick-index="7" aria-hidden="true" style="width: 140px;"
-                 tabindex="-1">
-              <h3>2</h3>
-            </div>
-            <div class="slick-slide slick-cloned" data-slick-index="8" aria-hidden="true" style="width: 140px;"
-                 tabindex="-1">
-              <h3>3</h3>
-            </div>
-            <div class="slick-slide slick-cloned" data-slick-index="9" aria-hidden="true" style="width: 140px;"
-                 tabindex="-1">
-              <h3>4</h3>
-            </div>
-          </div>
+  <div class="carousel">
+    <div class="carousel__content">
+      <div class="carousel__button-prev" @click="previousClick"></div>
+      <div class="carousel__scroll-area">
+        <div class="carousel__wrapper" :style="[baseStyle, otherStyle]">
+          <div class="carousel__slide carousel__slide-duplicate">Slide 3</div>
+          <div class="carousel__slide carousel__slide-duplicate">Slide 4</div>
+          <div class="carousel__slide carousel__slide-duplicate">Slide 5</div>
+          <div class="carousel__slide carousel__slide-duplicate">Slide 6</div>
+          <div class="carousel__slide">Slide 1</div>
+          <div class="carousel__slide">Slide 2</div>
+          <div class="carousel__slide">Slide 3</div>
+          <div class="carousel__slide">Slide 4</div>
+          <div class="carousel__slide">Slide 5</div>
+          <div class="carousel__slide">Slide 6</div>
+          <div class="carousel__slide carousel__slide-duplicate">Slide 1</div>
+          <div class="carousel__slide carousel__slide-duplicate">Slide 2</div>
+          <div class="carousel__slide carousel__slide-duplicate">Slide 3</div>
+          <div class="carousel__slide carousel__slide-duplicate">Slide 4</div>
         </div>
-        <button type="button" data-role="none" class="slick-next slick-arrow" aria-label="Next" role="button"
-                style="display: block;" @click="NextClick">Next
-        </button>
       </div>
+      <div class="carousel__button-next" @click="nextClick"></div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from "vue";
+import {ref, computed} from "vue";
+
+/**
+ * 展示为前面有 4 个作为过渡的滑块
+ * 后面 也有 4 个作为过渡的的滑动
+ * 这是在当滑块滑动到尽头时，先使用动画效果滑动到作为过渡的滑块的位置
+ * 然后动画结束后，迅速定位到真实展示位置的滑块的地方
+ */
+
 const translate3dX = ref(-560)
-const otherStyle = ref('')
+const otherStyle = ref({ transition: '' })
+
+// 当前展示的滑块数量
+const sliderLength = 6
+// 展示区域一次展示的滑块数量
+const showNumber = 4
+// 滑动的宽度
+const slideWidth = 140
+// 展示区域第一个滑块的位置
+const firstSlideShowPosition = -(showNumber * slideWidth)
+// 展示区域最后一个滑块的位置
+const lastSlideShowPosition = - (sliderLength * slideWidth)
+// 展示区域宽度
+const displayAreaWidth = showNumber * slideWidth
 
 const baseStyle = computed(() => ({
-  opacity: 1,
-  width: 1960 + 'px',
+  transitionDuration: '0ms',
   transform: `translate3d(${translate3dX.value}px, 0, 0)`
 }))
 
 const previousClick = () => {
-  otherStyle.value = { transition: `5s` }
-  translate3dX.value = 0
+  otherStyle.value.transition = 'transform .5s ease-in-out'
 
+  // 虚拟过渡位置
+  let fakeX = 0;
+  // 真实展示位置
+  let realX = 0;
+  // 已滑动到最前面
+  if (translate3dX.value === firstSlideShowPosition) {
+    // 虚拟滑块最初展示位置
+    fakeX = 0
+    realX = lastSlideShowPosition
+  } else {
+    // 未滑动到最前面，正常往前滑动
+
+    // 前面真实剩余的滑块数量
+    const frontRemainNumber = (translate3dX.value + displayAreaWidth) / slideWidth
+    // 若大于展示区域一次展示的滑块数量，则直接往前移动showNumber，否则直接展示firstSlideShowPosition
+    realX = frontRemainNumber >= showNumber ? translate3dX.value + displayAreaWidth : firstSlideShowPosition
+    fakeX = realX
+  }
+  console.log('previous - fake - real ? ', fakeX, realX)
+
+  translate3dX.value = fakeX
   setTimeout(() => {
     otherStyle.value.transition = ''
-    translate3dX.value = -840
-  }, 5000)
+    translate3dX.value = realX
+  }, 500)
 }
 
-const NextClick = () => {
-  translate3dX.value = -840
+const nextClick = () => {
+  otherStyle.value.transition = 'transform .5s ease-in-out'
+
+  // 虚拟过渡位置
+  let fakeX = 0;
+  // 真实展示位置
+  let realX = 0;
+
+  // 已滚动到最后一个
+  if (translate3dX.value === lastSlideShowPosition) {
+    // 虚拟滑块最后展示位置
+    fakeX = (sliderLength + showNumber) * -slideWidth
+    realX = firstSlideShowPosition
+  } else {
+    // 未滚动到最后一个，正常往后滚动
+
+    // 已滑动的滑块数量
+    const scrolledNumber = translate3dX.value / -slideWidth
+    // 最后剩余的未展示的滑块数量
+    const backRemainNumber = sliderLength - scrolledNumber
+    /**
+     * 若最后剩余的未展示的滑块数量大于可展示区域的滑块数量，
+     * 则
+     */
+    realX = backRemainNumber >= showNumber
+        ? translate3dX.value - displayAreaWidth
+        : firstSlideShowPosition + (-slideWidth * (showNumber - backRemainNumber))
+    fakeX = realX
+  }
+  console.log('next - fake - real ? ', fakeX, realX)
+
+  translate3dX.value = fakeX
+  setTimeout(() => {
+    otherStyle.value.transition = ''
+    translate3dX.value = realX
+  }, 500)
 }
 </script>
 
-<style>
-@import "index.css";
-
-.carousel-box {
-  display: flex;
-  justify-content: center;
-  align-content: center;
-}
-.carousel-content {
-  width: 560px;
-}
+<style lang="scss">
+@import "index.scss";
 </style>
