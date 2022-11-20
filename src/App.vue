62ad38ec5b6cf4c57e6e4db01dd557a618815611
binea <bineawu@163.com>
2022-11-20 16:01:26 +0800

    test changes 2

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@ const value1 = ref(true)
 const value2 = ref([30, 60])
 
 function handleSliderChange(val: any) {
-  console.log('handleSliderChange test branch changes = ', val)
+  console.log('handleSliderChange test branch changes 22222222222222222222222 = ', val)
 }
 
 function handleSliderInput(val: any) {
